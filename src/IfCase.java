f6279367fc6a1dba2a85e1ba1a41c2ebc6a3a2d5
Rodnei Dias <rodnei.dias04@gmail.com>
2024-04-09 14:09:47 -0300

    Ajuste do laço

--- src/IfCase.java
+++ src/IfCase.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class IfCase {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-
+    int opcao = 0;
         String linha="*************************************";
         String textoInicial = "Dados iniciais do cliente";
         String nomeCliente = "Rodnei Ferreira Dias Santos";
@@ -31,28 +31,33 @@ public class IfCase {
         System.out.println("");
         System.out.println(menuOperacoes);
 
-        int opcao = leitura.nextInt();
-        if (opcao == 1) {
-            System.out.println("Seu saldo atual é " + saldo);
-        }else if (opcao == 2) {
-            System.out.println("Insira o valor recebido");
-            double valorRecebido = leitura.nextInt();
-            double novoSaldo = saldo + valorRecebido;
-            System.out.println("Seu saldo atual é R$"+ novoSaldo);
-        }else if (opcao == 3) {
-            System.out.println("Insira o valor da transferência");
-            double valorTransferencia = leitura.nextInt();
-            if (valorTransferencia > saldo) {
-                System.out.println("Não há saldo suficiente para fazer essa transferência.");
+        while (opcao !=4){
+            System.out.println(menuOperacoes);
+            opcao = leitura.nextInt();
+
+
+            if (opcao == 1) {
+                System.out.println("Seu saldo atual é " + saldo);
+            }else if (opcao == 2) {
+                System.out.println("Insira o valor recebido");
+                double valorRecebido = leitura.nextInt();
+                double novoSaldo = saldo + valorRecebido;
+                System.out.println("Seu saldo atual é R$"+ novoSaldo);
+            }else if (opcao == 3) {
+                System.out.println("Insira o valor da transferência");
+                double valorTransferencia = leitura.nextInt();
+                if (valorTransferencia > saldo) {
+                    System.out.println("Não há saldo suficiente para fazer essa transferência.");
+                }else {
+                double novoSaldo = saldo - valorTransferencia;
+                System.out.println("Seu saldo atual é R$"+ novoSaldo);
+                }
+            }else if (opcao == 4) {
+                System.out.println("Obrigado por utilizar nossos serviços.");
+                leitura.close();
             }else {
-            double novoSaldo = saldo - valorTransferencia;
-            System.out.println("Seu saldo atual é R$"+ novoSaldo);
+                System.out.println("Opção inválida");
             }
-        }else if (opcao == 4) {
-            System.out.println("Obrigado por utilizar nossos serviços.");
-            leitura.close();
-        }else {
-            System.out.println("Opção inválida");
         }
     }
 }
